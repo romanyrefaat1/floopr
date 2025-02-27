b0bebc8950fd32f374631dd6c0d7b2cb4e80cb28
romanyrefaat1 <refaatromany641@gmail.com>
2025-02-27 23:50:30 +0200

    created sidebar

---
+++ app/(routes)/(code)/(need sidebar)/layout.tsx
@@ -0,0 +1,22 @@
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/app-sidebar";
+import { cookies } from "next/headers";
+
+export default async function Layout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const cookieStore = await cookies();
+  const defaultOpen = cookieStore.get("sidebar_state")?.value === "true";
+
+  return (
+    <SidebarProvider defaultOpen={defaultOpen}>
+      <AppSidebar />
+      <main>
+        <SidebarTrigger />
+        {children}
+      </main>
+    </SidebarProvider>
+  );
+}
