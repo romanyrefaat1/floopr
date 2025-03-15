5887fed0bf73b6662f626d001e95765a81018b91
romanyrefaat1 <refaatromany641@gmail.com>
2025-03-15 21:15:05 +0200

    Make secure

--- firebase.ts
+++ firebase.ts
@@ -4,13 +4,13 @@ import { getFirestore, collection, addDoc, query, where, getDocs, serverTimestam
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDe5DDaMyrl1zpu82IMVYdHUE81hZFoDxY",
-  authDomain: "ideaboar.firebaseapp.com",
-  projectId: "ideaboar",
-  storageBucket: "ideaboar.firebasestorage.app",
-  messagingSenderId: "476355972239",
-  appId: "1:476355972239:web:864e53883df26700bdb4fa",
-  measurementId: "G-E0DS66M4Q2"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
