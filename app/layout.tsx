5096ff356b62241b56c5843ca9a0cf5326ed28fb
romanyrefaat1 <refaatromany641@gmail.com>
2025-05-02 21:29:34 +0300

    Fix favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,18 @@ export const metadata = {
   title: "Floopr - Make User Feedback Actionable",
   description:
     "Collect, organize, and prioritize user feedback all in one place. Help your team build what users really want.",
+  icons: [
+    {
+      rel: "icon",
+      url: "/floopr-logo-no-bg-white-svg.svg",
+      media: "(prefers-color-scheme: dark)",
+    },
+    {
+      rel: "icon",
+      url: "/floopr-logo-no-bg-svg.svg",
+      media: "(prefers-color-scheme: light)",
+    },
+  ],
 };
 
 export default function RootLayout({
