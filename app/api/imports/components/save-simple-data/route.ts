7cc1f7bfb84200e6fc93fc83b3b9ade4ff257dd9
romanyrefaat1 <refaatromany641@gmail.com>
2025-05-08 14:26:05 +0300

    refactor: Update import path for addSimpleFeedback utility in save-simple-data route

--- app/api/imports/components/save-simple-data/route.ts
+++ app/api/imports/components/save-simple-data/route.ts
@@ -1,4 +1,4 @@
-import { addSimpleFeedback } from "@/packages/floopr-feedback/float-button/src/utils/add-feedback";
+import { addSimpleFeedback } from "@/actions/add-feedback";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
