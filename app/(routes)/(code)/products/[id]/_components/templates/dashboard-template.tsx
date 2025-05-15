b72572a7bf119035cb9e05b598a9c23a596ada69
romanyrefaat1 <refaatromany641@gmail.com>
2025-05-15 22:19:05 +0300

    Fix shadow in tab list in products/ id pa

--- app/(routes)/(code)/products/[id]/_components/templates/dashboard-template.tsx
+++ app/(routes)/(code)/products/[id]/_components/templates/dashboard-template.tsx
@@ -67,7 +67,7 @@ export default async function DashboardTemplate({
         <Tabs defaultValue="feedback" className="space-y-4">
           <TabsClient>
             {/* Tabs List - Scrollable on Small Screens */}
-            <div className="overflow-x-auto sticky top-0 z-[2] shadow-md">
+            <div className="overflow-x-auto sticky top-0 z-[2] shadow-md w-fit">
               <TabsList className="flex gap-2 bg-mutedBackground text-foreground w-fit min-w-max md:grid md:grid-cols-4 md:max-w-md scrollbar-hide hover:scrollbar-default [&::-webkit-scrollbar]:h-1.5 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-thumb]:bg-primary/40 [&::-webkit-scrollbar-track]:bg-transparent">
                 <TabsTrigger
                   value="feedback"
