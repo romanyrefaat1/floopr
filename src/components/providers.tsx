8ff3ca1d3ea24bdfb5b01bd9f823b029d15c6dc7
romanyrefaat1 <refaatromany641@gmail.com>
2025-05-19 13:04:13 +0300

    Use Feedback script in providers

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -2,7 +2,6 @@
 
 import { useScrollAnimation } from "../hooks/use-scroll-animation";
 import { AuthProvider } from "@/contexts/auth-context";
-import FlooprFloatingFeedbackButton from "@/packages/floopr-feedback/float-button/src/floating-feedback-button";
 import { ClerkProvider, useUser } from "@clerk/nextjs";
 import { Analytics } from "@vercel/analytics/react";
 import Script from "next/script";
@@ -45,7 +44,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
       <Toaster />
 
       {/* <FlooprFloatButtonScript /> */}
-      <FlooprFloatingFeedbackButton
+      <FlooprFloatButtonScript
         apiKey={process.env.PA_FLOAT_BUTTON_FLOOPR_API_KEY}
         productId={`31a4fd3d-615a-409c-97ee-bda48bbbb8e2`}
         componentId={`6e5b7f46-0488-4a85-8564-3f323338471f`}
